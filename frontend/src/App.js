56ff330072c3710fabdc38b4cc710fdaebf79a11
Vyma <mavanvy2023@gmail.com>
2023-12-22 10:21:36 +0700

    2 commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 const App = () => {
-  return <p>Welcome To ProShop</p>;
+  return (
+    <>
+      <Header />
+      <main className='py-3 '>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
