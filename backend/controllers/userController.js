fba008ce409a9f18651fe42a1deef18eb5a589fb
Vyma <mavanvy2023@gmail.com>
2023-12-28 12:20:23 +0700

    How do json web tokens work

---
+++ backend/controllers/userController.js
@@ -0,0 +1,70 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
+
+const authUser = asyncHandler(async (req, res) => {
+  const {email,password} = req.body;
+
+  const user = await User.findOne({email})
+
+  if(user && (await user.matchPassword(password))){
+    res.json({
+      _id: user._id,
+      name:user.name,
+      email:user.email,
+      isAdmin:user.isAdmin,
+    })
+  }else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+  
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.send('register user');
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send('logout user');
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('get user profile');
+});
+
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send('update user profile');
+});
+
+
+const getUsers = asyncHandler(async (req, res) => {
+  res.send('get users');
+});
+
+const getUserByID = asyncHandler(async (req, res) => {
+  res.send('get user by id');
+});
+
+
+
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send('delete user');
+});
+
+
+const updateUser = asyncHandler(async (req, res) => {
+  res.send('update user');
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserByID,
+    updateUser
+}
