1a90ef667cb9cfa7e696b546cab03fc39f06439a
Vyma <mavanvy2023@gmail.com>
2023-12-30 17:04:06 +0700

    Order API Slice & Start Order Screen

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import {
 } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -27,6 +28,8 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
 
 
 app.use(notFound);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 
 const router = createBrowserRouter(
@@ -48,6 +49,10 @@ const router = createBrowserRouter(
         <Route
           path='/payment'
           element={<PaymentScreen />}
+        />
+        <Route
+          path='/placeorder'
+          element={<PlaceOrderScreen />}
         />
       </Route>
     </Route>
